3b3d6185fd8acb1aa1645b79ed8b66843d746c46
jandie <eidnaj@live.nl>
2018-06-17 16:21:23 +0200

    Added some base dependencies and hello world material ui

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Button variant="contained" color="primary">
+                Hello World
+            </Button>
+        );
+    }
 }
 
 export default App;

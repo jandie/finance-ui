b072dbbbd40f69ae3f1e0c05126bf8ad3e32ae8d
jandie <eidnaj@live.nl>
2018-07-20 14:30:39 +0200

    Added actioncreators for payments

--- src/actions/payments.js
+++ src/actions/payments.js
@@ -5,6 +5,9 @@ import {createAuth, handleResponseError} from "./util";
 export const PAYMENTS_FETCHING = 'PAYMENTS_FETCHING';
 export const PAYMENTS_FETCHED = 'PAYMENTS_FETCHED';
 
+export const PAYMENT_FETCHED = 'PAYMENT_FETCHED';
+export const PAYMENT_DELETED = 'PAYMENT_DELETED';
+
 export const fetchPayments = (token) => dispatch => {
     const url = `${API_URL}payments/`;
 
@@ -18,6 +21,36 @@ export const fetchPayments = (token) => dispatch => {
             type: PAYMENTS_FETCHED,
             payload: res.data
         })
+    }).catch(error => {
+        handleResponseError(dispatch, error.response.status);
+    })
+};
+
+export const editPayment = (payment, token, callback = () => {}) => dispatch => {
+    const url = `${API_URL}payments/${payment.id}/`;
+
+    axios.put(url, payment, createAuth(token)).then(res => {
+        dispatch({
+            type: PAYMENT_FETCHED,
+            payload: payment
+        });
+
+        callback();
+    }).catch(error => {
+        handleResponseError(dispatch, error.response.status);
+    })
+};
+
+export const deletePayment = (id, token, callback = () => {}) => dispatch => {
+    const url = `${API_URL}payments/${id}/`;
+
+    axios.delete(url, createAuth(token)).then(res => {
+        dispatch({
+            type: PAYMENT_DELETED,
+            payload: id
+        });
+
+        callback();
     }).catch(error => {
         handleResponseError(dispatch, error.response.status);
     })

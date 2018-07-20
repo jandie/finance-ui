15936599b9c51e45fc3507adc517f80573c4c7e5
jandie <eidnaj@live.nl>
2018-07-20 14:31:00 +0200

    Reducer updated for payments

--- src/reducers/payments.js
+++ src/reducers/payments.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import {PAYMENTS_FETCHED, PAYMENTS_FETCHING} from "../actions/payments";
+import {PAYMENT_DELETED, PAYMENT_FETCHED, PAYMENTS_FETCHED, PAYMENTS_FETCHING} from "../actions/payments";
 
 export default (state={}, action) => {
     switch (action.type) {
@@ -12,6 +12,16 @@ export default (state={}, action) => {
             return {
                 payments: _.mapKeys(action.payload, 'id'),
                 fetching: false
+            };
+        case PAYMENT_FETCHED:
+            return{
+                payments: { ...state.payments, [action.payload.id]: action.payload },
+                fetching: state.fetching
+            };
+        case PAYMENT_DELETED:
+            return{
+                payments: _.omit(state.payments, action.payload),
+                fetching: state.fetching
             };
         default:
             return state;

0df118373844a45b3c3175f9a37498cbf7cd4066
jandie <eidnaj@live.nl>
2018-07-22 10:20:49 +0200

    Fixed calculation bug when automatically changing balance

--- src/actions/transactions.js
+++ src/actions/transactions.js
@@ -19,6 +19,7 @@ export const addTransaction = (transaction, paymentId, token, callback) => dispa
       dispatchSnack(dispatch, 'Transaction added');
       callback();
   }).catch(error => {
+      console.log(error);
       handleResponseError(dispatch, error.response,
           'Error: Adding transaction failed');
   })

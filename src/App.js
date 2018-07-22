cc10f52396aa75904dcafb4fb5e0440dd58182de
jandie <eidnaj@live.nl>
2018-07-22 12:02:27 +0200

    Added maxwidth for desktop

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, {Component} from 'react';
 import AppBar from './components/AppBar';
-import AppDrawer from './components/AppDrawer';
 import Snackbar from './components/Snackbar';
+import AppDrawer from "./components/AppDrawer";
 
-export default ({ children }) => {
-    return(
-        <div>
-            <AppBar/>
-            <AppDrawer/>
-            {children}
-            <Snackbar/>
-        </div>
-    );
+import {withStyles} from '@material-ui/core/styles';
+
+const styles = {
+    container: {
+        backgroundColor: 'inherit',
+        width: '100%',
+        height: '100%'
+    },
+    filler: {
+        height: '400px',
+        backgroundColor: 'inherit'
+    },
+    children: {
+        marginTop: '20px',
+        width: '100%',
+        maxWidth: '1200px',
+        marginLeft: 'auto',
+        marginRight: 'auto'
+    }
+};
+
+class App extends Component {
+    render() {
+        const {classes} = this.props;
+        return(
+            <div className={classes.container}>
+                <AppBar/>
+                <AppDrawer/>
+                <div className={classes.children}>
+                    {this.props.children}
+                    <div className={classes.filler}>
+                </div>
+
+                </div>
+                <Snackbar/>
+            </div>
+        );
+    }
 }
+
+export default withStyles(styles) (App);
